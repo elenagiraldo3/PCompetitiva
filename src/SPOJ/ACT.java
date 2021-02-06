e6c32a9f556a229080ab122b89811cdbc9120083
elenagiraldo3 <elenagiraldo3@gmail.com>
2021-02-06 19:46:52 +0100

    SPOJ ACT problem

--- src/SPOJ/ACT.java
+++ src/SPOJ/ACT.java
@@ -1,5 +1,19 @@
 package SPOJ;
 
+import java.util.Scanner;
+
 public class ACT {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine();
+        while (t > 0) {
+            String[] linea = sc.nextLine().split(" ");
+            System.out.println(linea[1].charAt(linea[1].length()-1));
+            t--;
+        }
+
+    }
     
 }

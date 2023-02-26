53ec0f33bdaf12f1913e3ea43a41f41ee2771b14
elenagiraldo3 <elenagiraldo3@gmail.com>
2023-02-26 17:53:49 +0100

    Problema 377: Cuadrados imperfectos

--- src/AER/P300_399/P377.java
+++ src/AER/P300_399/P377.java
@@ -1,15 +1,20 @@
 package AER.P300_399;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class P377 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while (sc.hasNextLong()) {
-            long n = sc.nextLong();
-            String h = Long.toHexString(n);
-            char mod = h.charAt(h.length()-1);
-            if ((mod=='0' || mod=='1' || mod=='4' || mod=='9')) {
+        BigInteger dieciseis = new BigInteger("16");
+        BigInteger cero = new BigInteger("0");
+        BigInteger uno = new BigInteger("1");
+        BigInteger cuatro = new BigInteger("4");
+        BigInteger nueve = new BigInteger("9");
+        while (sc.hasNextBigInteger()) {
+            BigInteger n = sc.nextBigInteger();
+            BigInteger modulo = n.mod(dieciseis);
+            if (modulo.equals(cero) || modulo.equals(uno) || modulo.equals(cuatro) || modulo.equals(nueve)) {
                 System.out.println("NO SE");
             } else {
                 System.out.println("IMPERFECTO");

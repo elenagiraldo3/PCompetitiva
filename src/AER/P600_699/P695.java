222294644d77b1ca49f7f9c04f3ab9307411ba82
Elena Giraldo del Viejo <elgidv.data@aquacorp.es>
2023-06-28 16:31:48 +0200

    Problema 695: Curiosa propiedad la del 9

---
+++ src/AER/P600_699/P695.java
@@ -0,0 +1,33 @@
+package AER.P600_699;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Objects;
+
+public class P695 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String N = bf.readLine();
+        while (!(N.length() == 1 && N.charAt(0) == '0')) {
+            int[] number = new int[N.length()];
+            for (int i=0; i<N.length(); i++) {
+                number[i] = Integer.parseInt(String.valueOf(N.charAt(i)));
+            }
+            Arrays.sort(number);
+            int small = 0;
+            int big = 0;
+            for (int i=0; i<number.length; i++) {
+               small *= 10;
+               big *= 10;
+               small += number[i];
+               big += number[number.length - 1 - i];
+            }
+            System.out.println(big + " - " + small + " = " + (big-small) + " = " + (big-small)/9 + " x 9");
+
+            N = bf.readLine();
+        }
+    }
+}

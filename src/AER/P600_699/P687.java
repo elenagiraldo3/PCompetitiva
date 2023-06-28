fb347c544ccb98d58dc64462036bb2b4ac826c0e
Elena Giraldo del Viejo <elgidv.data@aquacorp.es>
2023-06-28 16:30:40 +0200

    Problema 687: Anillos de plástico

---
+++ src/AER/P600_699/P687.java
@@ -0,0 +1,28 @@
+package AER.P600_699;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class P687 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int vertices = sc.nextInt();
+        while (vertices != 0) {
+
+            int aristas = 0;
+            int init = sc.nextInt();
+            int end = sc.nextInt();
+            while (!(init == 0 & end == 0)) {
+                init = sc.nextInt();
+                end = sc.nextInt();
+                aristas++;
+            }
+
+            System.out.println(aristas - vertices + 1);
+            vertices = sc.nextInt();
+
+        }
+    }
+
+}
